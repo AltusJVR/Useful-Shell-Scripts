51a4dfe7e6c11ab6dd5d336c4436c5fe75bc14f5
Altus Janse Van Rensburg <albertusbjvr@gmail.com>
2021-08-09 23:31:56 -0300

    first commit

---
+++ monitor.sh
@@ -0,0 +1,9 @@
+xrandr --newmode "1900" 170.75  1904 2024 2224 2544  1080 1083 1093 1120 -hsync>
+
+xrandr --addmode HDMI-1-1 "1900"
+
+
+xrandr --newmode "1800" 150.50  1800 1912 2096 2392  1012 1015 1025 1050 -hsync +vsync
+
+
+xrandr --addmode HDMI-1-1 "1800"
