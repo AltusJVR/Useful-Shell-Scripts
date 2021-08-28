505bfa230a3621d844de2dc098e7b48e4c8b159a
Altus Janse Van Rensburg <albertusbjvr@gmail.com>
2021-08-27 21:51:57 -0300

    update html starter

--- html-starter/html-starter.sh
+++ html-starter/html-starter.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/bash
+starterhtml="$HOME/Documents/Dev/Useful-Shell-Scripts/html-starter/index.html"
+startercss="$HOME/Documents/Dev/Useful-Shell-Scripts/html-starter/style.css"
+starterjs="$HOME/Documents/Dev/Useful-Shell-Scripts/html-starter/app.js"
 
-touch index.html &&
-touch app.js &&
-touch style.css && 
-code .
+if [ -f "$starterhtml" ]; then   
+  cat $starterhtml >> "index.html"
+fi
 
+if [ -f "$startercss" ]; then 
+  cat $startercss >> "style.css"
+fi
+
+if [ -f "$starterjs" ]; then 
+  cat $starterjs >> "app.js"
+fi
