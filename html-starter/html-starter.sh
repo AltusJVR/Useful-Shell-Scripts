46b35f500b4d63e1d85103600b89c5e7070280d6
Altus Janse Van Rensburg <albertusbjvr@gmail.com>
2021-08-31 01:36:03 -0300

    update custom monitor script

--- html-starter/html-starter.sh
+++ html-starter/html-starter.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/bash
-baseURL="$HOME/bin/html-start"
+baseURL="$HOME/bin/html-starter"
 starterhtml="$baseURL/index.html"
 startercss="$baseURL/style.css"
 starterjs="$baseURL/app.js"
 
-if [ -f "$starterhtml" ]; then   
-  cat $starterhtml >> "index.html"
+if  [ -f "$starterhtml" ]; then   
+  cat "$starterhtml" > "index.html" &&
+  echo "created index.html"
 fi
 
 if [ -f "$startercss" ]; then 
-  cat $startercss >> "style.css"
+  cat "$startercss" > "style.css" &&
+  echo "created style.css"
 fi
-
+&&
 if [ -f "$starterjs" ]; then 
-  cat $starterjs >> "app.js"
+  cat "$starterjs" > "app.js" &&
+  echo "created app.js"
 fi
